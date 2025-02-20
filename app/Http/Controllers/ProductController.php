fb843ca8190c054522ed50630e02a53279c0494b
Mark-laurencee <152530712+Mark-laurencee@users.noreply.github.com>
2025-02-20 12:45:36 +0000

    Implemented Anime List array in the controller & Displayed the data on the view in an organized manner and fixed passing of products array in controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function productMethod()
+    {
+        $products = [
+            ["name" => "Demon Slayer", "genre" => "Action"], ["name" => "Blue Lock", "genre" => "Sports"], ["name" => "Power Rangers", "genre" => "Action"], ["name" => "Akame ga Kill", "genre" => "Action"], ["name" => "One Piece", "genre" => "Adventure"],
+        ];
+
+        return view('product', compact('products'));
+    }
+}
---
+++ resources/views/product.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style> 
+        body {
+            margin: 50px;
+            align-items: center;
+            display: flex;
+            justify-content: center;
+            text-align: center;
+        }
+        table{
+            width: 50%;
+            margin: auto;
+            border: 1px solid black;
+            padding: 10px;
+            border-radius: 25px;
+        }
+        td, th{
+            border: 1px solid black;
+            padding: 10px;
+          
+        }
+        h1 {
+            margin-left:200px
+        }
+    </style>
+</head>
+<body>
+    <h1>Anime List:</h1>
+    <table>
+        <tr>
+            <th>Name of the Anime</th>
+            <th>Genre</th>
+        </tr>
+        @foreach($products as $product)
+        <tr>
+            <td>{{ $product['name'] }}</td>
+            <td>{{ $product['genre'] }}</td>
+        </tr>
+        @endforeach
+    </table>
+    
+</body>
+</html>
